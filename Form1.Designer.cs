76e22292fd88d117c29e1f316fa864bd9b724d45
sandra najera <sandra.najera626@gmail.com>
2026-06-03 10:38:27 -0600

    cambio 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(719, 159);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.Size = new System.Drawing.Size(156, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hola mundo";
+            this.label1.Text = "Prueba cambio";
             // 
             // Form1
             // 

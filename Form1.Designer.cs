1898095cc9b70d6a619c6f57c40eadf61034563e
Paolap0-0 <papperez598@gmail.com>
2026-06-03 12:07:20 -0600

    Hola Sandra

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,17 +34,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(719, 159);
+            this.label1.Location = new System.Drawing.Point(235, 176);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.Size = new System.Drawing.Size(75, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hola mundo";
+            this.label1.Text = "Hola Sandra :)";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1554, 588);
+            this.ClientSize = new System.Drawing.Size(1370, 588);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
--- Form1.cs
+++ Form1.cs
@@ -28,5 +28,15 @@ namespace Proyecto_Calificaciones
             //un Elefante se columpiaba sore la tela de una araña
             // Como veia que resistia fueron a llamar a otro elefante
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
